06130fc255979324542579c5d58e8c212647237d
adelelsawaf <adelsadeq24@gmail.com>
2026-08-15 16:07:08 +0300

    Refactor(backened-app-controller): improve app controller to return a more descriptive message

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Adeoula isweqa good boy';
+    return 'Hello, reviewer! Hope you enjoy going through this — it was a pleasure building it.';
   }
 }
